2d45eef8523c165e8400dde8288c356901c118b2
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-13 21:12:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,12 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).first
-  puts assign_rooms(attendees).first
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+
+  badges.each_with_index do |name, index|
+    puts name
+  end
 end
+
+
