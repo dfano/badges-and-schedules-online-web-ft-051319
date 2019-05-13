c7cf82e076c728f609fa804dfe0bf096ee883aa8
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-13 20:59:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,8 @@ def assign_rooms(attendees)
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
